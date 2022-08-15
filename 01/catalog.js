eefc79e4063fdb73e590b3b03b4b75b0df7f0831
qiangql <95154265+qiangql@users.noreply.github.com>
2022-08-15 18:05:47 +0800

    Add file catalog.js
    Move catalog mock data from index.html to clean it up a bit

---
+++ 01/catalog.js
@@ -0,0 +1,60 @@
+let catalog = {
+  "name" : "Seafood",
+  "desc" : "Delicious seafood direct to your home",
+  "productcategories": [
+    {
+      "id": 1,
+      "name": "Fish",
+      "description": "Fresh fishes",
+      "subcategories": [
+        {
+          "id": 101,
+          "name": "Salt water fish",
+          "description": "Fishes from the sea",
+          "products": [
+            {"id": 10101, "name": "Shark", "price": "8.99", "unit":  "lb", "inventory": 2000},
+            {"id": 10102, "name": "Belt fish", "price": "6.99", "unit":  "lb", "inventory": 1000},
+            {"id": 10103, "name": "Catfish", "price": "3.99", "unit":  "lb", "inventory": 1200}
+          ]
+        },
+        {
+          "id": 102,
+          "name": "White water fish",
+          "description": "Fishes from the rivers",
+          "products": [
+            {"id": 10201, "name": "Shark", "price": ".99", "unit":  "lb", "inventory": 2000},
+            {"id": 10202, "name": "Belt fish", "price": "6.99", "unit":  "lb", "inventory": 1000},
+            {"id": 10203, "name": "Catfish", "price": "3.99", "unit":  "lb", "inventory": 1200}
+          ]
+        }
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Crab",
+      "description": "Fresh crabs",
+      "subcategories": [
+        {
+          "id": 201,
+          "name": "Salt water crab",
+          "description": "Crabs from the sea",
+          "products": [
+            {"id": 20101, "name": "Shark", "price": "8.99", "unit":  "lb", "inventory": 2000},
+            {"id": 20102, "name": "Belt fish", "price": "6.99", "unit":  "lb", "inventory": 1000},
+            {"id": 20103, "name": "Catfish", "price": "3.99", "unit":  "lb", "inventory": 1200}
+          ]
+        },
+        {
+          "id": 202,
+          "name": "White water crab",
+          "description": "Crabs from the rivers",
+          "products": [
+            {"id": 20201, "name": "Shark", "price": "8.99", "unit":  "lb", "inventory": 2000},
+            {"id": 20202, "name": "Belt fish", "price": "6.99", "unit":  "lb", "inventory": 1000},
+            {"id": 20203, "name": "Catfish", "price": "3.99", "unit":  "lb", "inventory": 1200}
+          ]
+        }
+      ]
+    }
+  ]
+};
