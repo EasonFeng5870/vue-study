81ff80b6216d4a0b21b23ae9ebdc6a37dbbec86a
冯英胜 <fengyingsheng@dangdang.com>
2022-10-17 19:41:52 +0800

    feat:单品详情页

--- playground/vue-play/src/components/Product.vue
+++ playground/vue-play/src/components/Product.vue
@@ -18,7 +18,13 @@ export default {
     <!--img :src="product.imageurl" :alt="product.desc" width="20" height="20"/-->
     {{ product.id }} - {{ product.name }}
   </p>
+  <p>
+    -----details-----
+    {{ product.shortDesc }} - {{ product.fullDesc }} - {{product.iconUrl}}
+  </p>
 	<p>Price: ${{product.price}} per {{product.unit}}</p>
   <input type="number" min="1" max="100" v-model="this.quantity" size="3" />
   <button @click.stop="$emit('addtocart', product, quantity)">Add to cart</button>
+  <p></p>---
+  Debug:[product]: {{product}}
 </template>
